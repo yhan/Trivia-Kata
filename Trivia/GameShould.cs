56b0830913354fa0300927f37a8fb6ea228131a5
han <yhan@la-francaise.com>
2019-01-08 21:57:10 +0100

    Init Repo

---
+++ Trivia/GameShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trivia
+{
+    using NUnit.Framework;
+
+    using UglyTrivia;
+
+    class GameShould
+    {
+        [Test]
+        public void testcase()
+        {
+            Game aGame = new Game();
+            aGame.add("Chet");
+            aGame.roll(1);
+
+        }
+    }
+}
